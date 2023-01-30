db9f32905e28827fb2eebf25ec5009f0d89118d3
kwaw-kumi <kwawkumi9@gmail.com>
2023-01-30 15:09:28 -0800

    patch

--- routes/aliens.js
+++ routes/aliens.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const alien = require('../models/alien');
 const router = express.Router();
 const Alien = require('../models/alien');
 
 router.get('/', (req, res) =>{
    Alien.find()
+   .then((result) => {
+    res.json(result);
+   })
+   .catch((error) => {
+    res.send(error)
+   });
+})
+router.get('/:id', (req, res) =>{
+   Alien.findById(req.params.id)
    .then((result) => {
     res.json(result);
    })
@@ -28,5 +38,36 @@ router.post('/', async(req, res) => {
     });
     
 })
+router.put('/', async(req, res) => {
+    const alien = new Alien({
+        name: req.body.name,
+        tech: req.body.tech,
+        gender: req.body.gender,
+    }); 
+
+    alien.save()
+    .then((result) => {
+        res.json(result);
+    })
+    .catch((error) => {
+        res.send(error);
+    });
+    
+})
+
+
+
+// router.patch('/:id', async(req, res) => {
+//     Alien.findById(req.params.id)
+//         alien.tech = req.body.tech;
+
+//     alien.save()
+//     .then((result) => {
+//         res.json(result);
+//     })
+// .catch((error) => {
+//     res.send(error);
+// });
+// });
 // We export the router to makes it accessible in the app.js
 module.exports = router;
